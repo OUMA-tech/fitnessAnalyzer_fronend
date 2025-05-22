d5db73b157850a94c1619e53a86d78a0ada4c817
OUMA-tech <tianyfff11@gmail.com>
2025-05-22 21:05:01 +1000

    console env

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,9 @@ import AppRoutes from './AppRoutes.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
 
+console.log('ALL ENV:', import.meta.env);
+console.log('API URL:', import.meta.env.VITE_API_BASE_URL);
+console.log('MODE:', import.meta.env.MODE);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
